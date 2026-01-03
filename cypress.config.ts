b8b7a5220d3bfd7911cd1fbddca1444f6414cbc4
frosem <franklin.rosembluth@assuresoft.com>
2026-01-02 21:24:01 -0400

    feat: configure test frameworks for parallel execution
    - Playwright: read workers from package.json config in CI
    - Cypress: integrate cypress-split plugin for spec distribution

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "cypress";
 import { allureCypress } from "allure-cypress/reporter";
 import { getAllureConfig } from "@shared/config/allure.config";
+import cypressSplit from "cypress-split";
 
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.specs.ts',
     setupNodeEvents(on, config) {
+      cypressSplit(on, config);
       allureCypress(on, config, getAllureConfig('cypress'));
       require('@cypress/grep/plugin').plugin(config);
       return config;
